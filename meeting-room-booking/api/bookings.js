b4fbb50c6e5e1f647446d2993022faecb0b9d144
myselfabhi <abhinavverma2024@gmail.com>
2025-06-17 14:41:36 +0530

    fix: vercel ready layout for vercel

--- meeting-room-booking/api/bookings.js
+++ meeting-room-booking/api/bookings.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Booking = require ('../models/Booking');
+const Booking = require ('./lib/Booking');
 
 // GET all bookings
 router.get('/', async (req, res) => {
